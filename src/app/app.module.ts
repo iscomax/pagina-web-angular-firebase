37e3a6eadeb140192da7a96dd64bd6d240973146
iscomax <santamariajc61@gmail.com>
2018-07-18 22:21:01 -0500

    proyecto en angular version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    CarouselComponent,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule
